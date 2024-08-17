0cc7ad7674f2cf4398eee1dbbc45227794411a03
Michael Pavlov <micha.pavlov@gmail.com>
2024-08-17 21:31:59 +0300

    Fix camera movement

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -1,55 +1,61 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.UIElements;
 
-public class CameraMovement : MonoBehaviour
+namespace Camera
 {
-    [SerializeField]
-    private List<Transform> handsPositions;
-    [SerializeField]
-    private Transform background;
-    [SerializeField]
-    private float deltaBoundries = 2;
-
-    private float _startCameraYPosition;
-    private float _endCameraYPosition;
-
-    private float _startZoomValue = 5;
-    private float _endZoomValue = 8;
-
-
-    private Camera _camera;
-    private float _maxDistanceBetweenHandAndCastle;
-
-    private Vector3 _castlePosition;
-
-
-    void Start()
+    public class CameraMovement : MonoBehaviour
     {
-        _camera = GetComponent<Camera>();
-        _camera.orthographicSize = _startZoomValue;
-        _castlePosition = new Vector3(background.position.x, background.position.y - background.lossyScale.y / 2, 0);
-        Vector3 backgroundAngle = new Vector3(background.position.x + background.lossyScale.x / 2 - deltaBoundries, 
-                                            background.position.y - background.lossyScale.y / 2 - deltaBoundries, 0);
+        [SerializeField] private List<Transform> handsPositions;
+        [SerializeField] private SpriteRenderer background;
+        [SerializeField] private float deltaBoundries = 2;
 
-        _maxDistanceBetweenHandAndCastle = Vector3.Distance(_castlePosition, backgroundAngle);
+        private float _startCameraYPosition;
+        private float _endCameraYPosition;
 
-        _startCameraYPosition = background.position.y - 3;
-        _endCameraYPosition = background.position.y;
-        transform.position = new Vector3(background.position.x, _startCameraYPosition, -10);
-    }
+        private float _startZoomValue = 5;
+        private float _endZoomValue = 8;
 
-    void LateUpdate()
-    {
-        float currentDistanceBetweenHandsAndCastle = Mathf.Max(
-            Vector3.Distance(handsPositions[0].position, _castlePosition),
-            Vector3.Distance(handsPositions[1].position, _castlePosition));
-        float currentZoom = Mathf.Lerp(_startZoomValue, _endZoomValue, currentDistanceBetweenHandsAndCastle / _maxDistanceBetweenHandAndCastle);
-        _camera.orthographicSize = currentZoom;
 
-        float currentYPosition = Mathf.Lerp(_startCameraYPosition, _endCameraYPosition, currentZoom/_endZoomValue);
-        transform.position = new Vector3(background.position.x, currentYPosition, -10);
+        private UnityEngine.Camera _camera;
+        private float _maxDistanceBetweenHandAndCastle;
+
+        private Vector3 _castlePosition;
+
+
+        void Start()
+        {
+            _camera = GetComponent<UnityEngine.Camera>();
+            _camera.orthographicSize = _startZoomValue;
+
+            var backgroundPosition = background.transform.position;
+            var size = background.size;
+
+            _castlePosition = new Vector3(backgroundPosition.x, backgroundPosition.y - size.y / 2, 0);
+            Vector3 backgroundAngle = new Vector3(
+                backgroundPosition.x + size.x / 2 - deltaBoundries, 
+                backgroundPosition.y - size.y / 2 - deltaBoundries, 
+                0);
+
+            _maxDistanceBetweenHandAndCastle = Vector3.Distance(_castlePosition, backgroundAngle);
+
+            _startCameraYPosition = backgroundPosition.y - 3;
+            _endCameraYPosition = backgroundPosition.y;
+            transform.position = new Vector3(backgroundPosition.x, _startCameraYPosition, -10);
+        }
+
+        void LateUpdate()
+        {
+            float currentDistanceBetweenHandsAndCastle = Mathf.Max(
+                Vector3.Distance(handsPositions[0].position, _castlePosition),
+                Vector3.Distance(handsPositions[1].position, _castlePosition));
+
+            var progress = currentDistanceBetweenHandsAndCastle / _maxDistanceBetweenHandAndCastle;
+            
+            float currentZoom = Mathf.Lerp(_startZoomValue, _endZoomValue, progress);
+            _camera.orthographicSize = currentZoom;
+
+            float currentYPosition = Mathf.Lerp(_startCameraYPosition, _endCameraYPosition, progress);
+            transform.position = new Vector3(background.transform.position.x, currentYPosition, -10);
+        }
     }
 }
