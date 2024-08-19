7d644c243f993a7ff31f9b20d5761e26cb5b6810
Michael Pavlov <micha.pavlov@gmail.com>
2024-08-19 22:36:01 +0300

    Add ScrollWindow

---
+++ Assets/Scripts/UI/ScrollWindow.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using PrimeTween;
+using UnityEngine;
+
+namespace UI
+{
+    public class ScrollWindow : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup _canvasGroup;
+        [SerializeField] private RectTransform _scrollBody;
+        [SerializeField] private TweenSettings<float> _alphaTweenSettings;
+        [SerializeField] private TweenSettings<Vector2> _sizeTweenSettings;
+
+        private void Awake()
+        {
+            _canvasGroup.interactable = false;
+            _canvasGroup.alpha = _alphaTweenSettings.startValue;
+            _scrollBody.sizeDelta = _sizeTweenSettings.startValue;
+        }
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(0.5f);
+            Open();
+            yield return new WaitForSeconds(5.0f);
+            Close();
+        }
+
+        public void Open()
+        {
+            Sequence.Create()
+                .Chain(Tween.Alpha(_canvasGroup, _alphaTweenSettings))
+                .Chain(Tween.UISizeDelta(_scrollBody, _sizeTweenSettings))
+                .ChainCallback(() => _canvasGroup.interactable = true);
+        }
+        
+        public void Close()
+        {
+            Sequence.Create()
+                .ChainCallback(() => _canvasGroup.interactable = false)
+                .Chain(Tween.UISizeDelta(_scrollBody, _sizeTweenSettings.WithDirection(false)))
+                .Chain(Tween.Alpha(_canvasGroup, _alphaTweenSettings.WithDirection(false)));
+        }
+    }
+}
